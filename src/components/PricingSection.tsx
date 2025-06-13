0f19235a6e6c1f98dc5ba933bacc266c8ba78e5c
Kamraan Mohammed <shoveb1@gmail.com>
2025-06-13 18:49:39 -0500

    Deploy changes for 2024-07-10 (second deployment)

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -139,7 +139,7 @@ const PricingSection = () => {
             <div className="bg-primary text-white p-8 mt-auto">
               <div className="flex items-end">
                 <span className="text-lg">from</span>
-                <span className="text-6xl font-bold ml-2">$5</span>
+                <span className="text-6xl font-bold ml-2">$4</span>
                 <span className="text-xl ml-1">/ hour</span>
               </div>
               <div className="mt-4 text-sm space-y-1">
